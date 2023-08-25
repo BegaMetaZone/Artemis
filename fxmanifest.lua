0d658468ce8184c31ac26956e2cd7357dc458efa
Bega <139539636+BegaMetaZone@users.noreply.github.com>
2023-08-25 14:37:21 +0330

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,22 @@
 fx_version 'adamant'
 game 'gta5'
 
-name "RaceMenu"
-description "RaceMenu powered by ScaleformUI project , a standalone menu to manage races , leaderboards , vehicles , customizations and much more"
+name "Artemis"
+description "Project Artemis powered by ScaleformUI project , a standalone menu to manage races , leaderboards , vehicles , customizations and much more"
 author "BegaMetaZone" 
-version "1.0.0"
+version "1.0.1"
 
-files { "carcols_gen9.meta", "carmodcols_gen9.meta" }
-data_file "CARCOLS_GEN9_FILE" "carcols_gen9.meta"
-data_file "CARMODCOLS_GEN9_FILE" "carmodcols_gen9.meta"
+files { "carcols_gen9.meta", "data/carmodcols_gen9.meta" }
+data_file "CARCOLS_GEN9_FILE" "data/carcols_gen9.meta"
+data_file "CARMODCOLS_GEN9_FILE" "data/carmodcols_gen9.meta"
 
-shared_script {'config.lua'}
+shared_script {'conf/*.lua'}
 
 client_scripts {
 	"@ScaleformUI_Lua/ScaleformUI.lua",
-	"RaceMenu.lua",
-	"Functions.lua"
+	"client/*.lua"
 }
 
 server_scripts {
-	"Server.lua"
+	"server/*.lua"
 }
